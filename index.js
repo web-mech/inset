ce3860a2dc4069431aba79140a483477a7d195a9
webmech <webmech@gmail.com>
2019-07-15 15:01:29 -0400

    fix(*): update regex for tokenizing vars
    allows for properly parsing vars that are snake-case

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function inset(tpl, ctx) {
-  return tpl.replace(/(?=\{)\{[a-z|A-Z|0-9|.]{0,}\}/g, function(match) {
+  return tpl.replace(/(?=\{)\{[\w|.]{0,}\}/g, function(match) {
     return match.replace(/[\{|\}]/g, '').split('.').reduce(function(ctx, property) {
       if (ctx && ctx.hasOwnProperty(property)) {
         return ctx[property];
